deb31c2f7d05552555e12778a3a8d3b9e1914239
Szymon Abramczyk <szymon.abramczykk@gmail.com>
2022-12-13 16:45:59 +0100

    aktualizacja o wybór elementu głównego

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,7 @@
 #include "gauss.h"
 
+#include <math.h>
+
 /**
  *  * Zwraca 0 - elimnacja zakonczona sukcesem
  *   * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -15,16 +17,37 @@ int eliminate(Matrix *mat, Matrix *b){
 
 	for( c = 0; c < n - 1; c++)
 	{
-		for( r = c+1; r < n; r++)
+		int cmax = c;
+
+		for( r = c+1; r < n; r++){
+			if( fabs(mat->data[r][c] > fabs(mat->data[cmax][c])) ) 
+				cmax = r;
+		}
+
+		if( cmax != c )
 		{
+			double *tmp = mat->data[c];
+			mat->data[c] = mat->data[cmax];
+			mat->data[cmax] = tmp;
+
+			double btmp = b->data[c][0];
+			b->data[c][0] = b->data[cmax][0];
+			b->data[cmax][0] = btmp;
+		}
+		
+
+		for( r = c+1; r < n; r++)
+		{ 
+
 			if( mat->data[c][c] == 0 )
-																			return 1;
+				return 1;
 			double m = mat->data[r][c] / mat->data[c][c];
-																						
-																		for( i = c; i < n; i++ )
-																			mat->data[r][i] -= m * mat->data[c][i];
-																		b->data[r][0] -= m * b->data[c][0];
-																	}
+			
+			for( i = c; i < n; i++ )
+				mat->data[r][i] -= m * mat->data[c][i];
+	
+			b->data[r][0] -= m * b->data[c][0];
+		}	
 	}
-		return 0;
+	return 0;
 }
